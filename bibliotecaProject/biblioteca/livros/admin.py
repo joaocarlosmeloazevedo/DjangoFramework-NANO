f8582728cf37ad86049df15725f4f86f8136c33f
joaocarlosmeloazevedo <joaocarlos.mazevedo12@gmail.com>
2021-11-08 12:26:35 -0300

    Apps Livro e Autores criados e configurados.

--- bibliotecaProject/biblioteca/livros/admin.py
+++ bibliotecaProject/biblioteca/livros/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Livro
 
+class ExibeLivro(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'ano_publicacao', 'isbn')
+    list_display_links = ('id', 'titulo')
+    search_fields = ('titulo',) #Virgula necessaria
+    list_filter = ('ano_publicacao',) #Virgula necessaria
+    list_per_page = 2
+
+admin.site.register(Livro, ExibeLivro)
 # Register your models here.
--- bibliotecaProject/biblioteca/livros/models.py
+++ bibliotecaProject/biblioteca/livros/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from autores.models import Autor
 
-# Create your models here.
+class Livro(models.Model):
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE) #FK -> Vínculo | on_delete...
+    # exclui autor e livro ou vice-versa em modo cascada. Influencia o vinculo das tabelas.
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    paginas = models.IntegerField()
+    editora = models.CharField(max_length=50)
+    edicao = models.IntegerField()
+    ano_publicacao = models.IntegerField()
+    idioma = models.CharField(max_length=20)
+    isbn = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.titulo
